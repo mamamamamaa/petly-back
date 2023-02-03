cb0aaf602ca1f59d175d9bd1fa3832938215160b
Yulia Semenyuk <semenyukyp@gmail.com>
2023-02-03 14:18:38 +0200

    user's own pet model

---
+++ models/usersOwnPetModel.js
@@ -0,0 +1,26 @@
+const { Schema, model, } = require("mongoose");
+
+const ownPetSchema = new Schema ({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+    },
+    dateOfBirth: {
+        type: Date,
+    },
+    breed: {
+        type: String,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+    },
+},
+    {versionKey: false}
+);
+
+const ownPet = model("ownPet", ownPetSchema);
+
+module.exports = {ownPet};
+
