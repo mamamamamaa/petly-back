37537e69bbd499ef030a094babbf8d00860c0ee1
Nikolay Tankov <tankov.n.k@gmail.com>
2023-02-04 13:29:14 +0200

    getOnlyYourNotice

--- routes/notice.js
+++ routes/notice.js
@@ -1,6 +1,11 @@
 const route = require("express").Router();
 const authenticate = require("../../middlewares/authenticate");
-const { getNoticeById, updateNoticeFavorite } = require("../controllers/notice")
+const {
+  getOnlyYourNotice,
+  getNoticeById,
+  updateNoticeFavorite,
+} = require("../controllers/notice");
 
-route.get('/:noticeId', authenticate, getNoticeById)
-route.patch('/:noticeId/favorite', authenticate, updateNoticeFavorite);
+route.get("/", authenticate, getOnlyYourNotice);
+route.get("/:noticeId", authenticate, getNoticeById);
+route.patch("/:noticeId/favorite", authenticate, updateNoticeFavorite);
