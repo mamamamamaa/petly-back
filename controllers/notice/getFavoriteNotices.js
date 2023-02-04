248d798326ea05bfe30f47b1c21863532a921836
ProskurinV <proskyrinv.g@gmail.com>
2023-02-04 23:17:41 +0200

    getFix

--- controllers/notice/getFavoriteNotices.js
+++ controllers/notice/getFavoriteNotices.js
@@ -2,6 +2,7 @@ const { Notice } = require("../../models/notice");
 
 const getFavoriteNotices = async (req, res) => {
   const { _id } = req.user;
+  const { favorite } = req.query;
 
   const favNotices = await Notice.find({ owner: _id, favorite }).populate(
     "owner"
