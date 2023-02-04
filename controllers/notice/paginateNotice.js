a53c32fdb02d18ba8a1dc0d08a52e83d220bf6d2
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-04 06:21:26 +0200

    paginateNotice

---
+++ controllers/notice/paginateNotice.js
@@ -0,0 +1,28 @@
+const { Notice } = require('../../models/Notice');
+
+const HttpError = require('../middlewares');
+const ctrlWrapper = require('../helpers');
+
+const paginateNotice = async (req, res) => {
+  const { page = 1, limit = 8 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Notice.find(
+    {},
+    {},
+    {
+      skip,
+      limit: Number(limit),
+    },
+  );
+  if (!result) {
+    throw HttpError(404);
+  }
+
+  res.status(200).json({
+    message: `${result}`,
+  });
+};
+
+module.exports = {
+  paginateNotice: ctrlWrapper(paginateNotice),
+};
