9d01db7ff79d352855004f2daa09ea4b5a2fd576
Olga Sumaruk <olga_sumaruk@hotmail.com>
2023-02-05 02:15:20 +0200

    add sort by date (new first)

--- controllers/news/news.js
+++ controllers/news/news.js
@@ -2,10 +2,10 @@ const News = require("../../models/news");
 
 const getNews = async (req, res) => {
     
-    const {page = 1, limit = 6} = req.query;
+    const {page = 1, limit = 20} = req.query;
     const skip = (page - 1) * limit;
     
-    const allNews = await News.find({}, "-createdAt -updatedAt", {skip, limit: Number(limit)});
+    const allNews = await News.find({}, "-createdAt -updatedAt", {skip, limit: Number(limit)}).sort({ date: -1 });
 
     res.json({
         status: "success",
