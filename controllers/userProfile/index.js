7d8af5479392d4e349e31ef81bd4f192f732e48b
mamamamamaa <maks.denishchuk@gmail.com>
2023-02-05 20:36:21 +0200

    ///

--- controllers/userProfile/index.js
+++ controllers/userProfile/index.js
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
 const { addOwnPet } = require("./addOwnPet");
 const { deleteOwnPet } = require("./deleteOwnPet");
 const { getUserProfile } = require("./getUserProfile");
-const { updateAvatar } = require("./updateAvatar");
 const { ctrlWrapper } = require("../../helpers");
 
 module.exports = {
   addOwnPet: ctrlWrapper(addOwnPet),
   deleteOwnPet: ctrlWrapper(deleteOwnPet),
   getUserProfile: ctrlWrapper(getUserProfile),
-  updateAvatar: ctrlWrapper(updateAvatar),
 };
-=======
-const {addOwnPet} = require('./addOwnPet');
-const {deleteOwnPet} = require('./deleteOwnPet');
-const {getUserProfile} = require('./getUserProfile');
-
-
-module.exports = {
-    addOwnPet,
-    deleteOwnPet,
-    getUserProfile
-};
->>>>>>> 99802c14787721c47817d703fa88f8a3a832c47b
