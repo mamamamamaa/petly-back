e84c163390625a7467b75e42b4a6fd749731bc3b
Olga Sumaruk <olga_sumaruk@hotmail.com>
2023-02-05 02:54:55 +0200

    not found by query

--- controllers/news/news.js
+++ controllers/news/news.js
@@ -1,3 +1,4 @@
+const { HttpError } = require("../../middlewares");
 const News = require("../../models/news");
 
 const getNews = async (req, res) => {
@@ -5,8 +6,12 @@ const getNews = async (req, res) => {
     const {page = 1, limit = 6, query = ''} = req.query;
     const skip = (page - 1) * limit;
     const condition = query === '' ? {} : { description: {$regex: query} };
-    
+
     const allNews = await News.find(condition, "-createdAt -updatedAt", {skip, limit: Number(limit)}).sort({ date: -1 });
+
+    if(allNews.length === 0){
+        throw HttpError(404);
+    }
 
     res.json({
         status: "success",
