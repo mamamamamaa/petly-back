b86cbae6e3316214ba6ec0b89c2d5cc3627a5c3c
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-06 07:16:22 +0200

    delete comments

--- controllers/notice/updateNoticeFavorite.js
+++ controllers/notice/updateNoticeFavorite.js
@@ -1,55 +1,25 @@
-// const schemaUpdateNoticeFavorite = require("../../schemas/updateNoticeFavorite");
-const {Notice} = require("../../models/notice");
-
+const { Notice } = require('../../models/notice');
 
 const updateNoticeFavorite = async (req, res) => {
-    const { noticeId } = req.params;
-    // const { favorite = false } = req.body;
-    const { _id: userId } = req.user;
-    // if (!favorite) {
-    //     return res.status(400).json({
-    //         status: 'error',
-    //         code: 400,
-    //         message: 'missing field favorite',
-    //     })
-    // }
+  const { noticeId } = req.params;
 
-    // const { error} = schemaUpdateNoticeFavorite.validate({ favorite });
-    // if (error) {
-    //     return res.status(400).json({ message: "missing required name field" });
-    // }
-    const result = await Notice.findByIdAndUpdate(noticeId, {
-      $addToSet: {
-        favorite: userId,
-      },
-    }).exec();
-    if (result) {
-        res.json(result)
-    }
-    else {
-        res.status(404).json({
-            status: 'error',
-            code: 404,
-            message: `Not found contact id: ${noticeId}`,
-            data: 'Not Found',
-        })
-    }
-}
+  const { _id: userId } = req.user;
 
-//   const result = await Notice.findByIdAndUpdate({ _id: noticeId }, value, {
-//     returnOriginal: false,
-//   });
-
-//   if (result) {
-//     res.json(result);
-//   } else {
-//     res.status(404).json({
-//       status: "error",
-//       code: 404,
-//       message: `Not found contact id: ${noticeId}`,
-//       data: "Not Found",
-//     });
-//   }
-// };
+  const result = await Notice.findByIdAndUpdate(noticeId, {
+    $addToSet: {
+      favorite: userId,
+    },
+  }).exec();
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(404).json({
+      status: 'error',
+      code: 404,
+      message: `Not found contact id: ${noticeId}`,
+      data: 'Not Found',
+    });
+  }
+};
 
 module.exports = { updateNoticeFavorite };
