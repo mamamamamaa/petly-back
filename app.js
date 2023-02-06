a8c315fad0749188c4900946243f01ed114fbe53
mamamamamaa <maks.denishchuk@gmail.com>
2023-02-06 21:29:29 +0200

    ///

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger_output.json');
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
 
 require("dotenv").config();
 
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 const { HOST } = process.env;
-
 
 const authRouter = require("./routes/api/auth");
 
@@ -22,7 +20,6 @@ const userRouter = require("./routes/api/userProfile");
 const newsRouter = require("./routes/api/news");
 const noticesRouter = require("./routes/api/notices");
 const friendsRouter = require("./routes/api/friends");
-
 
 app.use(cors());
 app.use(logger(formatsLogger));
@@ -38,13 +35,11 @@ app.use("/lostFound", lostFoundRouter);
 
 app.use("/api/userprofile", userRouter);
 app.use("/api/news", newsRouter);
-// app.use("/api/notices", noticesRouter);
+app.use("/api/notices", noticesRouter);
 
 app.use("/api/friends", friendsRouter);
 
-
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 mongoose.set("strictQuery", true);
 mongoose.connect(HOST, () => console.log("DB is connect"));
