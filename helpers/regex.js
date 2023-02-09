708618285206f9fdf0a57e0985808d4933770803
mamamamamaa <maks.denishchuk@gmail.com>
2023-02-09 22:12:17 +0200

    update password regex

--- helpers/regex.js
+++ helpers/regex.js
@@ -1,7 +1,7 @@
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const passwordRegex = /^\S*$/;
+const passwordRegex = /^[A-Za-z0-9]\S*$/;
 
 const mobilePhoneRegex = /^\+?3?8?(0\d{2}\d{3}\d{2}\d{2})$/;
 
