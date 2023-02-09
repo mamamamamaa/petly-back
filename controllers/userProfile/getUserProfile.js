5f65b7ed3a331592aaa7d52b1fdc7904ec844641
Yulia Semenyuk <semenyukyp@gmail.com>
2023-02-09 22:51:17 +0200

    ...

--- controllers/userProfile/getUserProfile.js
+++ controllers/userProfile/getUserProfile.js
@@ -4,7 +4,7 @@ const getUserProfile = async (req, res) => {
   const { _id: owner } = req.user;
   const user = req.user;
   const ownPets = await OwnPet.find({ owner });
-  res.json({ user, pets: ownPets });
+  res.json({data: {user, pets: ownPets }});
 };
 
 module.exports = {
