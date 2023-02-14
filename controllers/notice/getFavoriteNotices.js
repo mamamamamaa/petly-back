ddcdffa1e11624ba0f3233543bbe47bf4df763f0
mamamamamaa <maks.denishchuk@gmail.com>
2023-02-14 12:34:12 +0200

    //

--- controllers/notice/getFavoriteNotices.js
+++ controllers/notice/getFavoriteNotices.js
@@ -1,28 +1,24 @@
 const { Notice } = require("../../models/notice");
 const { HttpError } = require("../../middlewares");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const getFavoriteNotices = async (req, res) => {
   const { favorite } = req.user;
-  const {
-    page = 1,
-    limit = 20,
-  } = req.query;
+  const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
 
   console.log(!Number.isNaN(page));
 
   if (page < 1 || limit < 1) {
-    throw HttpError(400)
+    throw HttpError(400);
   }
-  if (!Number.isNaN(page) || !Number.isNaN(limit)) {
-    throw HttpError(400, "Page or limit isn't number")
+  if (Number.isNaN(page) || Number.isNaN(limit)) {
+    throw HttpError(400, "Page or limit isn't number");
   }
 
   if (limit > 100) {
-    throw HttpError(400, "Page limit max 100")
+    throw HttpError(400, "Page limit max 100");
   }
-
 
   if (!favorite) {
     return res.status(400).json({
@@ -32,9 +28,11 @@ const getFavoriteNotices = async (req, res) => {
     });
   }
 
-  const favNotices = await Notice.find({ _id: { $in: favorite.map((notice) => mongoose.Types.ObjectId(notice)) } }, "-createdAt -updatedAt", { skip, limit })
-
-  res.json(favNotices)
+  const favNotices = await Notice.find(
+    { _id: { $in: favorite.map((notice) => mongoose.Types.ObjectId(notice)) } },
+    "-createdAt -updatedAt",
+    { skip, limit }
+  );
 
   if (!favNotices) {
     throw HttpError(404);
