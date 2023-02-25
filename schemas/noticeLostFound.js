2f4bc5111115e0324308b6f9a7121041728b5024
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-25 04:51:05 +0200

    .allow("") Joi

--- schemas/noticeLostFound.js
+++ schemas/noticeLostFound.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const addNoticeLostFoundSchema = Joi.object({
   title: Joi.string().min(2).max(20).required(),
   name: Joi.string().min(1).max(20),
-  dateOfBirth: Joi.date(),
+  dateOfBirth: Joi.date().allow(""),
   breed: Joi.string().min(2).max(16),
   sex: Joi.string().valid("male", "female").required(),
   place: Joi.string().min(3).max(16).required(),
