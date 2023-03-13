c8fd8e2980a02e1f5e81582969180d1ace2cc9b3
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-03-13 18:57:10 +0200

    existUser and cors

--- app.js
+++ app.js
@@ -1,53 +1,57 @@
-const express = require("express");
-const cors = require("cors");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger_output.json");
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 const { HOST } = process.env;
 
-const authRouter = require("./routes/api/auth");
+const authRouter = require('./routes/api/auth');
 
-const userRouter = require("./routes/api/userProfile");
-const newsRouter = require("./routes/api/news");
-const noticesRouter = require("./routes/api/notices");
-const friendsRouter = require("./routes/api/friends");
+const userRouter = require('./routes/api/userProfile');
+const newsRouter = require('./routes/api/news');
+const noticesRouter = require('./routes/api/notices');
+const friendsRouter = require('./routes/api/friends');
 
 app.use(
-  cors({
+  cors(
+    {
     origin: ['http://localhost:3000', 'https://petly-front.onrender.com'],
     methods: 'GET,POST,PUT,DELETE',
-  })
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    }
+  )
 );
 app.use(logger(formatsLogger));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // ROUTES:
-app.use("/api/auth", authRouter);
+app.use('/api/auth', authRouter);
 
-app.use("/api/userprofile", userRouter);
-app.use("/api/news", newsRouter);
-app.use("/api/notices", noticesRouter);
+app.use('/api/userprofile', userRouter);
+app.use('/api/news', newsRouter);
+app.use('/api/notices', noticesRouter);
 
-app.use("/api/friends", friendsRouter);
+app.use('/api/friends', friendsRouter);
 
-app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-mongoose.set("strictQuery", true);
-mongoose.connect(HOST, () => console.log("DB is connect"));
+mongoose.set('strictQuery', true);
+mongoose.connect(HOST, () => console.log('DB is connect'));
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
+  const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
 module.exports = app;
--- controllers/auth/googleRedirect.js
+++ controllers/auth/googleRedirect.js
@@ -8,15 +8,13 @@ const {
   calculateExpiresTime,
 } = require('../../helpers');
 const { EXPIRES_IN } = process.env;
-const { GOOGLE_AUTH_PASSWORD } = process.env;
-
+const mongoose = require('mongoose');
 const googleRedirect = async (req, res) => {
   // response from google
   const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
   const urlObj = new URL(fullUrl);
   const urlParams = querystring.parse(urlObj.search); // urlObj.search - google access params from big object
   const code = urlParams['?code']; // urlParams all google settings, code for access
-  // check code by debugger if no undefined, if no empty string
   const tokenData = await axios({
     url: `https://oauth2.googleapis.com/token`,
     method: 'post',
@@ -39,29 +37,32 @@ const googleRedirect = async (req, res) => {
   const email = userData.data.email;
   const user = await User.findOne({ email });
   if (!user) {
-    const hashPassword = await bcrypt.hash(GOOGLE_AUTH_PASSWORD, 10);
+    const hashPassword = await bcrypt.hash(tokenData.data.access_token, 10);
+    const _id = await mongoose.Types.ObjectId();
     await User.create({
       password: hashPassword,
       verify: true,
-      mobilePhone: '',
-      city: '',
-      name: '',
+      mobilePhone: 'incognito',
+      city: 'incognito',
+      name: 'incognito',
       favorite: [],
+      verificationToken: 'incognito',
+      email,
+      _id,
+      accessToken: generateAccessToken(_id),
+      refreshToken: generateRefreshToken(_id),
     });
   }
-  const { password, city, name, mobilePhone, _id, favorite } = user;
-  const accessToken = generateAccessToken(user);
-  const refreshToken = generateRefreshToken(user);
-  await User.findByIdAndUpdate(user._id, { accessToken, refreshToken });
+  const existUser = await User.findOne({ email });
+  const { password, city, name, mobilePhone, _id, favorite } = existUser;
+  const accessToken = generateAccessToken(existUser);
+  const refreshToken = generateRefreshToken(existUser);
+  await User.findByIdAndUpdate(existUser._id, { accessToken, refreshToken });
   const expiresIn = calculateExpiresTime(EXPIRES_IN);
   return res.redirect(
     // redirect user to frontend
     `${process.env.CLIENT_URL}?accessToken=${accessToken}&refreshToken=${refreshToken}&expiresIn=${expiresIn}&password=${password}&email=${email}&name=${name}&mobilePhone=${mobilePhone}&city=${city}&id=${_id}&favorite=${favorite}`
   );
-  
 };
 module.exports = googleRedirect;
-//   // point to the access_token token in query params
-  //   // google-redirect to follow user NOT to empty string on frontend
-  //   // `${process.env.FRONTED_URL}/google-redirect/?accessToken=${accessToken}&refreshToken=${refreshToken}`,
-  // );
+ 
